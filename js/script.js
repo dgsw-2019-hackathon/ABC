502e15c1edad329de3829be82129e4751528084d
Eun Chong Ju <vevsing@gmail.com>
2019-07-16 17:59:19 +0900

    Add for filter search function.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function(){
+	$(".app-g").hide();
+	
 	$(".sub").hide();
 	// $("ul > li:first-child a").next().show();
 	$(".navi>li a").click(function(){
@@ -9,8 +11,75 @@ $(document).ready(function(){
 		return false;
 	});
 	$(".navi>li a").eq(0).trigger("click");
+	
+	$(".search-btn").click(function(){
+		var age = $("#age").val();
+		var gender = $("#gender").val();
+		var genre = $("#genre").val();
+		
+		hideAll();
+		
+//		console.log("::"+age+gender+genre);
+		
+		var v = valueCodeResult(age, gender, genre);
+		$('.'+v).show();
+	});
 });
 
+function valueCodeResult(a, s, g){
+	var result = "";
+	
+	if (a == "A10"){
+		result=result+"A1";
+	}
+	else if (a == "A20"){
+		result=result+"A2";
+	}
+	else if (a == "A30"){
+		result=result+"A3";
+	}
+	else if (a == "A40"){
+		result=result+"A4";
+	}
+	
+	if (s == "M"){
+		result=result+"M";
+	}
+	else if (s == "F"){
+		result=result+"F";
+	}
+	
+	if (g == "GAME"){
+		result=result+"GM";
+	}
+	else if (g == "MUSIC"){
+		result=result+"MS";
+	}
+	else if (g == "SOCIAL"){
+		result=result+"SC";
+	}
+	else if (g == "FINANCIAL"){
+		result=result+"FN";
+	}
+	
+//	console.log(result);
+	
+	return result;
+}
+
+function hideAll(){
+	$(".app-g").hide();
+}
+
+function showAlltoA(){
+	
+}
+function showAlltoS(){
+	
+}
+function showAlltoG(){
+	
+}
 
 //
 //function openNav(){
@@ -21,3 +90,4 @@ $(document).ready(function(){
 //	$(".left-list").css({"width":"0px", "display":"none"});
 //}
 //
+
