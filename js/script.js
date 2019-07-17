cf69e4819cfc6bc496db4a3a2b4e5cf26a907df6
kimmin-gi <40197230+kimmin-gi@users.noreply.github.com>
2019-07-17 10:41:22 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -91,14 +91,15 @@ function openNav(){
 	}
 	else if (window.matchMedia("(min-width: 800px)").matches) {
 		/* 뷰포트 너비가 800 픽셀 이상 */
-		$(".left-list").css({"width":"80%"});
+		$(".left-list").css({"width":"60%"});
 	}
 	else {
 		/* 뷰포트 너비가 800 픽셀 미만 */
-		$(".left-list").css({"width":"90%"});
+		$(".left-list").css({"width":"40%"});
 	}
 }
 
 function closeNav(){
 	$(".left-list").css({"width":"0px"});
 }
+
